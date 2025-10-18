d6ddbe8c8cef7cd358fa3fcf877c6c506a44c29c
Nathan-MDSS <nathanmdss2002@gmail.com>
2025-10-18 19:38:32 +0000

    Introduzi a lógica da peça "Cavalo", com duas - estruturas de repetição aninhadas (For && While)

--- xadrez.c
+++ xadrez.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-int main(){
-
-    
+int main()
+{
     int escolhaPeca;
-    int torre = 1, bispo = 1; 
-    int rainha;
-    // int vezesTorre, vezesBispo, vezesRainha;
-    
+    int torre = 1, bispo = 1;
+   /// int rainha;
+
     printf("===Desafio Nível Novato===\n");
     printf("===Movimentando peças de Xadrez!===\n");
     printf("Escolha uma peça para movimentar\n");
     printf("1. Torre\n");
     printf("2. Bispo\n");
     printf("3. Rainha\n");
-    printf("4. Sair.\n");
+    printf("4. Cavalo.\n");
+    printf("5. Sair.\n");
     scanf("%d", &escolhaPeca);
 
-    switch (escolhaPeca) {
-        case 1: 
+    switch (escolhaPeca)
+    {
+    case 1:
         printf("Você escolheu Torre!\n");
-       // printf("Selecione quatas casas e para qual direção você quer mover (atenção 1 a 5!)\n");
-       // scanf("%d", &vezesTorre);
         while (torre <= 5)
         {
             printf("Movendo a torre %d casa(s) para a direta!\n", torre);
@@ -29,34 +27,45 @@ int main(){
         }
         break;
 
-        case 2:
+    case 2:
         printf("Você escolheu Bispo!\n");
-        do {
+        do
+        {
             printf("Movendo o bispo %d casa(s) para cima e à direita!\n", bispo);
-            bispo++;            
-        } while (bispo <=5);
+            bispo++;
+        } while (bispo <= 5);
         break;
 
-        case 3:
-        for (rainha = 1; rainha <=8 ; rainha++) {
+    case 3:
+        for (int rainha = 1; rainha <= 8; rainha++)
+        {
             printf("Movendo a rainha %d casa(s) para a esquerda!\n", rainha);
         }
         break;
 
-        case 4:
+    case 4: 
+            for (int i = 1; i <= 2; ++i) {
+        printf("Cavalo se moveu %d casa(s) para baixo\n", i);
+        printf("\n");
+
+        while (i == 2 ) {
+            printf("Cavalo se moveu 1 casa(s) para a esquerda\n");
+            i = 3;
+    /*       poderia usar break aqui, mas na segunda volta do loop,
+          qnd break == 2, i nunca seria 3 geraria loop infinito. o break serviria para parar  
+          porém eu atualizei o valor de i dentro do while, pq o for automaticamente para qnd i for 3.
+          */
+        }
+    }
+    break;
+    case 5:
         printf("Você escolheu sair...\n");
         break;
 
-        default:
+    default:
         printf("Entrada Inválida...");
         return 0;
-    } 
-
-
-   
-
+    }
     printf("Fim do programa!");
-
     return 0;
-
 }
